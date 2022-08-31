bda22cdd1e8f88125cb20731971789ae32a9f829
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2022-08-31 12:52:14 +0700

    refactor code home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,149 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:teangan_film_mobile/app/modules/home/controllers/home_controller.dart';
-import 'package:teangan_film_mobile/app/resources/global/clipper.dart';
+import 'package:teangan_film_mobile/app/resources/components/wrapper.dart';
 import 'package:teangan_film_mobile/app/resources/global/colors.dart';
-import 'package:teangan_film_mobile/app/resources/global/images.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Row(
-          children: [
-            Container(
-              width: 24,
-              height: 24,
-              margin: const EdgeInsets.only(right: 10),
-              child: SvgPicture.asset(
-                Images.logo,
-                color: ColorItem.white,
+    return WrapperContent(
+      height: 260,
+      content: [
+        Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 20,
+          ),
+          width: Get.width,
+          height: 200,
+          color: Colors.grey[350],
+          child: Center(
+            child: Text(
+              "Slider",
+              style: TextStyle(
+                color: ColorItem.black,
               ),
-            ),
-            const Text('Teangan Film'),
-          ],
-        ),
-        actions: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(
-                  right: 16,
-                ),
-                width: 36,
-                height: 36,
-                child: CircleAvatar(
-                  radius: 10,
-                  backgroundImage: AssetImage(Images.userAvatar),
-                ),
-              ),
-            ],
-          )
-        ],
-        backgroundColor: ColorItem.red,
-        elevation: 0,
-      ),
-      body: Stack(
-        children: [
-          ClipPath(
-            clipper: RoundedClipper(),
-            child: Container(
-              width: Get.width,
-              height: 210,
-              color: ColorItem.red,
             ),
           ),
-          Column(
-            children: [
-              Divider(
-                thickness: 2,
-                height: 2,
-                color: ColorItem.red5,
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 18,
-                  vertical: 24,
-                ),
-                child: Column(
-                  children: [
-                    Text(
-                      "Teangan Film",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24,
-                        color: ColorItem.white,
-                      ),
-                    ),
-                    const SizedBox(height: 12),
-                    Container(
-                      constraints: const BoxConstraints(
-                        maxWidth: 420,
-                        minHeight: 40,
-                      ),
-                      child: Text(
-                        "Teangan Film is project for submission of learn the basic of flutter app in dicoding. This is simple app using API from TMDB (The Movie Database).",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: ColorItem.white,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 18,
-                ),
-                decoration: BoxDecoration(
-                  color: ColorItem.white,
-                  borderRadius: BorderRadius.circular(6),
-                ),
-                constraints: const BoxConstraints(
-                  maxWidth: 420,
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Search...',
-                          border: InputBorder.none,
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: ColorItem.red,
-                          ),
-                          isDense: true,
-                          contentPadding: const EdgeInsets.all(0),
-                        ),
-                        textAlignVertical: TextAlignVertical.center,
-                      ),
-                    ),
-                    Container(
-                      width: 1,
-                      height: 26,
-                      color: Colors.grey[300],
-                    ),
-                    IconButton(
-                      icon: Icon(
-                        Icons.keyboard_arrow_right,
-                        color: ColorItem.red,
-                      ),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/app/resources/components/wrapper.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:teangan_film_mobile/app/resources/global/clipper.dart';
+import 'package:teangan_film_mobile/app/resources/global/colors.dart';
+import 'package:teangan_film_mobile/app/resources/global/images.dart';
+
+class WrapperContent extends StatelessWidget {
+  final String? name;
+  final String? description;
+  final double? height;
+  final List<Widget>? content;
+
+  const WrapperContent({
+    Key? key,
+    this.name,
+    this.description,
+    this.height,
+    required this.content,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: SafeArea(
+        child: Stack(
+          children: [
+            ClipPath(
+              clipper: RoundedClipper(),
+              child: Container(
+                width: Get.width,
+                height: height ?? 320,
+                color: ColorItem.red,
+              ),
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  HeaderContent(
+                    name: name ?? 'Rizal Arfiyan',
+                    description: description ?? 'Get more the new movie!',
+                  ),
+                  ...content ?? <Widget>[],
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HeaderContent extends StatelessWidget {
+  final String name;
+  final String description;
+
+  const HeaderContent({
+    Key? key,
+    required this.name,
+    required this.description,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 20,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RichText(
+                text: TextSpan(
+                  text: "Hello ",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              RichText(
+                text: TextSpan(
+                  text: description,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 42,
+            height: 42,
+            child: CircleAvatar(
+              radius: 10,
+              backgroundImage: AssetImage(Images.userAvatar),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
